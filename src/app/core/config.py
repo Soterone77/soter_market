2b3b87347f5802c80ea91e6e406f29b239be2122
Nikita Shakhov <supersoter@yandex.ru>
2025-05-23 15:41:34 +0300

    ADD S3Client

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -41,6 +41,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(
             os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env"
